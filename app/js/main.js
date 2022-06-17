b9ac7f62a303e7a63fe2804dbe3c30927582c112
SergiyLuchko <LuchkoSergiy85@gmail.com>
2022-06-17 13:50:43 +0300

    add Terms & Conditions page

--- app/js/main.js
+++ app/js/main.js
@@ -8,10 +8,27 @@ $('.hero__slider-items').slick({
   fade:true
 });
 
+$(".accordion__btn").on("click", function(e) {
+
+  e.preventDefault();
+  var $this = $(this);
+
+  if (!$this.hasClass("accordion-active")) {
+    $(".accordion__content").slideUp(400);
+    $(".accordion__btn").removeClass("accordion-active");
+  
+  }
+  $this.toggleClass("accordion-active");
+  $this.next().slideToggle();
+
+});
+
 var mixer = mixitup('.tabs');
 var mixer = mixitup('.design__tabs');
 
 });
 $(function(){
   require('fslightbox');
+  
+  
 });
