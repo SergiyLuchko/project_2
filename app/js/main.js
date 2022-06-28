8085a64c5c333779ab023560e7abd6f4d8db831f
SergiyLuchko <LuchkoSergiy85@gmail.com>
2022-06-28 12:16:38 +0300

    add product-details page

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,19 @@
 $(function(){
 
+$('.details-tabs__btn').on('click', function(e){
+e.preventDefault();
+$('.details-tabs__btn').removeClass('details-tabs__btn--active');
+$(this).addClass('details-tabs__btn--active');
+$('.details-tabs__content-item').removeClass('details-tabs__content-item--active');
+$($(this).attr('href')).addClass('details-tabs__content-item--active')
+});
 
+
+
+$('.details__number').styler();
   $('.shop-category__label').on('click', function(){
-    
-    $('.products__panel').toggleClass('products__panel--list')
-   
-
-
-    });
+      $('.products__panel').toggleClass('products__panel--list')
+     });
 
 
   $(".shop-price__input").ionRangeSlider({
@@ -25,11 +31,39 @@ $(function(){
     spacing: "10px"
   });
   
+$('.related-slider__inner').slick({
+  arrows:true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  centerMode:true,
+  prevArrow:'<button type="button" class="slick-prev">    <img src="images/icons/prev.svg" alt=""></button>',
+  nextArrow:'<button type="button" class="slick-next">    <img src="images/icons/next.svg" alt=""></button>'
+});
+
 $('.hero__slider-items').slick({
   arrows:false,
   dots:true,
   autoplay:true,
   fade:true
+});
+$('.details-slider__thumbs').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.details-slider__big',
+  dots: true,
+ 
+  focusOnSelect: true,
+  draggable: false,
+  vertical: true
+});
+$('.details-slider__big').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  draggable: false,
+  
+  asNavFor: '.details-slider__thumbs'
 });
 
 $(".accordion__btn").on("click", function(e) {
